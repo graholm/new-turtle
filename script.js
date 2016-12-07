23d321d54fa43f499ee8c741f4d075e395c3c372
graholm <graholm@nuevaschool.org>
2016-12-07 09:01:54 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ $("body").keydown(function(keydownEvent) {
   console.log(keydownEvent.keyCode)
     if (keydownEvent.keyCode===13) {
       console.log ("start") 
-    goForward(100);
-    turnLeft();
       goForward(100);
       turnLeft();
       goForward(100);
       turnLeft();
       goForward(100);
       turnLeft();
+      goForward(100);  goForward(100);
+
      }
 })
 
