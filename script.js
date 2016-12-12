904b743be9459b2b3683c4a27af852a3165b2a9b
graholm <graholm@nuevaschool.org>
2016-12-12 08:43:35 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $("body").keydown(function(keydownEvent) {
   console.log(keydownEvent.keyCode)
-  if (keydownEvent.keyCode===13) {
+  if (keydownEvent.keyCode===39) {
+    turnLeft();
       goForward(150);
       turnLeft();
       goForward(150);
@@ -8,12 +9,12 @@ $("body").keydown(function(keydownEvent) {
 })
 
 
-function polygon (distance, n) {
-  for (i=0; i<n; i++){
-    goForward(distance)
-    turn(n)
-}}
-
-polygon (100, 10)
-
+//function polygon (distance, n) {
+//  for (i=0; i<n; i++){
+//    goForward(distance)
+//    turn(n)
+//}}
+//
+//polygon (100, 10)
+//
 
