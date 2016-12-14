eb2baa82561aeeb605ada29ceeaaca03d8d7389e
graholm <graholm@nuevaschool.org>
2016-12-14 09:50:59 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,24 @@
+//$("body").keydown(function(keydownEvent) {
+//  if (keydownEvent.keyCode===39) {
+//    goForward(100) 
+//  }
+//  else if
+//    (keydownEvent.keyCode===37) {
+//      turn()
+//      goForward (100)
+//      turn()
+//    }
+//})
+//
+
+
+
+function polygon (distance, n) {
+  for (i=0; i<n; i++){
+    goForward(distance)
+    turn(n)
+}}
+
+polygon (100, 3)
+
+
---
+++ turtles.js
@@ -0,0 +1,90 @@
+var heading = 0
+var position = {x: 200, y: 200};
+var turtle = $("#turtle");
+var turtleCage = $(".turtle-cage")
+
+function turnRight() {
+  heading = heading + 90  
+}
+function turn() {
+  heading = heading + 180  
+}
+
+function turnLeft() {
+  heading = heading - 90  
+}
+
+function turnLeft() {
+  heading = heading - 90  
+}
+
+function turn(n) {
+  heading = heading + 360/n 
+}
+
+
+
+
+
+function goForward(distance) {   
+  var radians = -1 * heading * (Math.PI/180);
+  var yIncrement = Math.sin(radians) * distance;
+  var xIncrement = Math.cos(radians) * distance;        
+
+  var origPosition = {x: position.x, y: position.y}; 
+  var origHeading = heading;
+
+  position.x = position.x + xIncrement;
+  position.y = position.y + yIncrement;
+
+  turtle.animate({
+    left: position.x, 
+    bottom: position.y,
+  },{
+    duration: 20,
+    complete : function() {
+      insertLine(origPosition, origHeading, distance)
+    }
+  })
+
+}
+
+function insertLine(start, angle, distance) {
+  var lineEl = $("<div class='line'></div>")
+  lineEl.css({
+    "left": start.x,
+    "bottom": start.y,
+    "width": distance,
+    height: "1px",
+    "transform-origin": "0 0 0",
+    transform: "rotate(" + angle + "deg)",
+    background: "black",
+    position: "absolute"
+  })
+  
+  
+  turtleCage.append(lineEl)
+}
+
+//function goBack(distance) {   
+//  var radians = -1 * heading * (Math.PI/180);
+//  var yIncrement = Math.sin(radians) * distance;
+//  var xIncrement = Math.cos(radians) * distance;        
+//
+//  var origPosition = {x: position.x, y: position.y}; 
+//  var origHeading = heading;
+//
+//  position.x = position.x + xIncrement;
+//  position.y = position.y + yIncrement;
+//
+//  turtle.animate({
+//    left: position.x, 
+//    bottom: position.y,
+//  },{
+//    duration: 20,
+//    complete : function() {
+//      insertLine(origPosition, origHeading, distance)
+//    }
+//  })
+//
+//}
